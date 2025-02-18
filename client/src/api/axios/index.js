99f7672093eb2c1b173f85d0cd7277ea66e2673c
Musallam Jawish <Musallamjawish314@gmail.com>
2025-02-18 12:48:53 +0300

    ctrl

--- client/src/api/axios/index.js
+++ client/src/api/axios/index.js
@@ -22,27 +22,32 @@ axiosInstance.interceptors.request.use(
     return Promise.reject(error);
   });
 
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   function (error) {
-//     if (error.response) {
-//       const status = error.response.status;
-//       if (status === 400) {
-//         window.location = "/login";
-//       }
-//       if (status === 500) {
-//         window.location = "/server-error";
-//       }
-//     } else if (error.request) {
-//       alert("Network error. Please check your connection.");
-//     } else {
-//       alert("An unexpected error occurred.");
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    if (error.response) {
+      const status = error.response.status;
+      if (status === 401) {
+        // Handle token expiration
+        window.location = "/login";
+      } else if (status === 400) {
+        // Handle bad requests
+        return Promise.reject(error);
+      } else if (status === 500) {
+        // Handle server errors
+        window.location = "/server-error";
+      }
+    } else if (error.request) {
+      // Handle network errors
+      alert("Network error. Please check your connection.");
+    } else {
+      // Handle other errors
+      alert("An unexpected error occurred.");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
-
