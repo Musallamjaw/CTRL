e1f7a1d2dad8f52a3fcc95b4deda596e0786b50d
Praveenkurupati <138180628+Praveenkurupati@users.noreply.github.com>
2025-02-26 00:21:06 +0530

    chnages for college and major selection register page

--- client/src/components/organism/SignUpForm.jsx
+++ client/src/components/organism/SignUpForm.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
-import { Formik, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { signUp, verifyCode } from '../../api/endpoints/auth';
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Formik, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { signUp, verifyCode } from "../../api/endpoints/auth";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const SignUpForm = () => {
   const [isVerificationStep, setIsVerificationStep] = useState(false);
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
+  const [selectedCollege, setSelectedCollege] = useState("");
   const navigate = useNavigate();
 
   const callSignUp = async (signUpData) => {
@@ -16,12 +17,14 @@ const SignUpForm = () => {
     try {
       const response = await signUp(signUpData);
       if (response.status === 200) {
-        toast.success('Verification code sent to your email.');
+        toast.success("Verification code sent to your email.");
         setEmail(signUpData.email);
         setIsVerificationStep(true);
       }
     } catch (error) {
-      toast.error(error?.response?.data?.message || 'Sign Up failed. Please try again.');
+      toast.error(
+        error?.response?.data?.message || "Sign Up failed. Please try again."
+      );
     } finally {
       setLoading(false);
     }
@@ -32,47 +35,52 @@ const SignUpForm = () => {
     try {
       const response = await verifyCode(verificationData);
       if (response.status === 201) {
-        toast.success('Verification successful. You are now registered!');
-        navigate('/logIn');
+        toast.success("Verification successful. You are now registered!");
+        navigate("/logIn");
       }
     } catch (error) {
-      toast.error(error?.response?.data?.message || 'Verification failed. Please try again.');
+      toast.error(
+        error?.response?.data?.message ||
+          "Verification failed. Please try again."
+      );
     } finally {
       setLoading(false);
     }
   };
 
   const initialValues = {
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    phoneNumber: '',  // حقل رقم الهاتف الجديد
-    verificationCode: '',
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    phoneNumber: "",
+    college: "",
+    major: "",
+    verificationCode: "",
   };
 
   const validationSchema = Yup.object({
     name: Yup.string()
-      .max(50, 'Must be 50 characters or less')
-      .required('Required'),
-    email: Yup.string()
-      .email('Invalid email address')
-      .required('Required'),
+      .max(50, "Must be 50 characters or less")
+      .required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
     password: Yup.string()
-      .min(8, 'Password must be at least 8 characters')
-      .required('Required'),
+      .min(8, "Password must be at least 8 characters")
+      .required("Required"),
     confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Passwords must match')
-      .required('Required'),
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Required"),
     phoneNumber: Yup.number()
-      .typeError('Phone number must be a number')
-      .required('Required'),  // التحقق من رقم الهاتف
+      .typeError("Phone number must be a number")
+      .required("Required"),
+    college: Yup.string().required("Required"),
+    major: Yup.string().required("Required"),
   });
 
   const verificationSchema = Yup.object({
     verificationCode: Yup.string()
-      .length(6, 'Code must be 6 digits')
-      .required('Required'),
+      .length(6, "Code must be 6 digits")
+      .required("Required"),
   });
 
   const handleSubmit = async (values, { setSubmitting }) => {
@@ -84,17 +92,40 @@ const SignUpForm = () => {
     setSubmitting(false);
   };
 
+  const majors = {
+    "College of Computing and Systems": [
+      "Computer Systems Engineering",
+      "Software Engineering",
+      "Cyber Security Engineering",
+      "Data Science and Artificial Intelligence",
+    ],
+    "College of Engineering and Energy": [
+      "Bio-Resources and Agricultural Engineering",
+      "Biomedical and Instruments Engineering",
+      "Energy Systems Engineering",
+      "Environmental Engineering and Sustainability",
+      "Material Science and Engineering",
+      "Robotics and Mechatronics Engineering",
+    ],
+    "College of Business & Entrepreneurship": [
+      "Entrepreneurship and Innovation",
+      "Digital Marketing",
+      "Supply Chain and Logistics Management",
+    ],
+  };
+
   return (
     <Formik
       initialValues={initialValues}
-      validationSchema={!isVerificationStep ? validationSchema : verificationSchema}
+      validationSchema={
+        !isVerificationStep ? validationSchema : verificationSchema
+      }
       onSubmit={handleSubmit}
     >
-      {({ getFieldProps, isSubmitting }) => (
+      {({ getFieldProps, setFieldValue, isSubmitting }) => (
         <Form className="flex flex-col gap-4 mb-20">
           {!isVerificationStep ? (
             <>
-              {/* نموذج إدخال بيانات المستخدم */}
               <div>
                 <label htmlFor="name" className="block mb-2 font-medium">
                   Full Name
@@ -102,10 +133,14 @@ const SignUpForm = () => {
                 <input
                   id="name"
                   type="text"
-                  {...getFieldProps('name')}
+                  {...getFieldProps("name")}
                   className="w-full min-h-12 p-3 pl-5 border rounded-md focus:outline-none focus:ring-2 focus:ring-base-color"
                 />
-                <ErrorMessage name="name" component="div" className="text-red-500 text-sm mt-1" />
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className="text-red-500 text-sm mt-1"
+                />
               </div>
 
               <div>
@@ -115,10 +150,14 @@ const SignUpForm = () => {
                 <input
                   id="email"
                   type="email"
-                  {...getFieldProps('email')}
+                  {...getFieldProps("email")}
                   className="w-full min-h-12 p-3 pl-5 border rounded-md focus:outline-none focus:ring-2 focus:ring-base-color"
                 />
-                <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-sm mt-1"
+                />
               </div>
 
               <div>
@@ -128,10 +167,14 @@ const SignUpForm = () => {
                 <input
                   id="phoneNumber"
                   type="text"
-                  {...getFieldProps('phoneNumber')}
+                  {...getFieldProps("phoneNumber")}
                   className="w-full min-h-12 p-3 pl-5 border rounded-md focus:outline-none focus:ring-2 focus:ring-base-color"
                 />
-                <ErrorMessage name="phoneNumber" component="div" className="text-red-500 text-sm mt-1" />
+                <ErrorMessage
+                  name="phoneNumber"
+                  component="div"
+                  className="text-red-500 text-sm mt-1"
+                />
               </div>
 
               <div className="grid md:grid-cols-2 gap-6">
@@ -142,41 +185,113 @@ const SignUpForm = () => {
                   <input
                     id="password"
                     type="password"
-                    {...getFieldProps('password')}
+                    {...getFieldProps("password")}
                     className="w-full min-h-12 p-3 pl-5 border rounded-md focus:outline-none focus:ring-2 focus:ring-base-color"
                   />
-                  <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-red-500 text-sm mt-1"
+                  />
                 </div>
 
                 <div>
-                  <label htmlFor="confirmPassword" className="block mb-2 font-medium">
+                  <label
+                    htmlFor="confirmPassword"
+                    className="block mb-2 font-medium"
+                  >
                     Confirm Password
                   </label>
                   <input
                     id="confirmPassword"
                     type="password"
-                    {...getFieldProps('confirmPassword')}
+                    {...getFieldProps("confirmPassword")}
                     className="w-full min-h-12 p-3 pl-5 border rounded-md focus:outline-none focus:ring-2 focus:ring-base-color"
                   />
-                  <ErrorMessage name="confirmPassword" component="div" className="text-red-500 text-sm mt-1" />
+                  <ErrorMessage
+                    name="confirmPassword"
+                    component="div"
+                    className="text-red-500 text-sm mt-1"
+                  />
                 </div>
+              </div>
+              <div>
+                <label htmlFor="college" className="block mb-2 font-medium">
+                  College
+                </label>
+                <select
+                  id="college"
+                  {...getFieldProps("college")}
+                  onChange={(e) => {
+                    setFieldValue("college", e.target.value);
+                    setSelectedCollege(e.target.value);
+                    setFieldValue("major", "");
+                  }}
+                  className="w-full min-h-12 p-3 pl-5 border rounded-md focus:outline-none focus:ring-2 focus:ring-base-color"
+                >
+                  <option value="">Select College</option>
+                  {Object.keys(majors).map((college) => (
+                    <option key={college} value={college}>
+                      {college}
+                    </option>
+                  ))}
+                </select>
+                <ErrorMessage
+                  name="college"
+                  component="div"
+                  className="text-red-500 text-sm mt-1"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="major" className="block mb-2 font-medium">
+                  Major
+                </label>
+                <select
+                  id="major"
+                  {...getFieldProps("major")}
+                  className="w-full min-h-12 p-3 pl-5 border rounded-md focus:outline-none focus:ring-2 focus:ring-base-color"
+                  disabled={!selectedCollege}
+                >
+                  <option value="">Select Major</option>
+                  {selectedCollege &&
+                    majors[selectedCollege].map((major) => (
+                      <option key={major} value={major}>
+                        {major}
+                      </option>
+                    ))}
+                </select>
+                <ErrorMessage
+                  name="major"
+                  component="div"
+                  className="text-red-500 text-sm mt-1"
+                />
               </div>
             </>
           ) : (
             <>
               {/* نموذج إدخال كود التحقق */}
               <div>
-                <label htmlFor="verificationCode" className="block mb-2 font-medium">
+                <label
+                  htmlFor="verificationCode"
+                  className="block mb-2 font-medium"
+                >
                   Verification Code
                 </label>
                 <input
                   id="verificationCode"
                   type="text"
-                  {...getFieldProps('verificationCode')}
+                  {...getFieldProps("verificationCode")}
                   className="w-full min-h-12 p-3 pl-5 border rounded-md focus:outline-none focus:ring-2 focus:ring-base-color"
                 />
-                <ErrorMessage name="verificationCode" component="div" className="text-red-500 text-sm mt-1" />
-                <p className="text-lg font-semibold text-base-color mt-4">Check Your Email</p>
+                <ErrorMessage
+                  name="verificationCode"
+                  component="div"
+                  className="text-red-500 text-sm mt-1"
+                />
+                <p className="text-lg font-semibold text-base-color mt-4">
+                  Check Your Email
+                </p>
               </div>
             </>
           )}
@@ -185,10 +300,17 @@ const SignUpForm = () => {
             <button
               type="submit"
               disabled={isSubmitting || loading} // تعطيل الزر أثناء التحميل
-              className={`w-full py-3 font-semibold rounded-md transition-all duration-300 ${loading ? 'bg-gray-400 cursor-not-allowed' : 'bg-base-color text-white hover:bg-second-color'
-                }`}
+              className={`w-full py-3 font-semibold rounded-md transition-all duration-300 ${
+                loading
+                  ? "bg-gray-400 cursor-not-allowed"
+                  : "bg-base-color text-white hover:bg-second-color"
+              }`}
             >
-              {loading ? 'Loading...' : isVerificationStep ? 'Verify Code' : 'Sign Up'}
+              {loading
+                ? "Loading..."
+                : isVerificationStep
+                ? "Verify Code"
+                : "Sign Up"}
             </button>
           </div>
         </Form>
--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
+    major: { type: String, required: true },
+    college: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phoneNumber: {type: Number, required: true},
+    phoneNumber: { type: Number, required: true },
     password: { type: String, required: true },
-    role: { type: String, default: 'user' },
-    avatar: { type: String }
-}, { timestamps: true });
+    role: { type: String, default: "user" },
+    avatar: { type: String },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
