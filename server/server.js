5e8b77caacfe47b6ad7486b3484adfb25eb8f042
Praveenkurupati <138180628+Praveenkurupati@users.noreply.github.com>
2025-02-26 15:23:03 +0530

    `Update CORS options to remove localhost origin`

--- server/server.js
+++ server/server.js
@@ -20,11 +20,7 @@ app.use(cookieParser());
 
 // CORS options - adjust origin for Render or dynamic front-end URL
 const corsOptions = {
-  origin: [
-    "https://ctrl-club.com",
-    "https://www.ctrl-club.com",
-    "http://localhost:5173",
-  ],
+  origin: ["https://ctrl-club.com", "https://www.ctrl-club.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
