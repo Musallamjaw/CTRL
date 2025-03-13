23f4649b562a886090a839c35dd6ad0f86783dd9
Musallamjaw <159564383+Musallamjaw@users.noreply.github.com>
2025-03-13 20:18:35 +0300

    Update Ticket.controller.js

--- server/controller/Ticket.controller.js
+++ server/controller/Ticket.controller.js
@@ -48,7 +48,6 @@ const sendEmail = async (email, subject, tickets) => {
     .map(
       (ticket, index) => `
     <div style="border:1px solid #000; color:white; padding:10px; margin-bottom:10px; max-width:300px; border-radius: 1rem; text-align:center; background-color: rgb(20, 16, 44);">
-      <h2>${ticket.eventData.title}</h2>
       <p>Location: ${ticket.eventData.location}</p>
       <img src="cid:image${
         index + 1
