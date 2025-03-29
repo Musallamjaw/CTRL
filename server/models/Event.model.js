73dcb7ba73dac82be6afd13c1529b91d3db070da
Musallamjaw <159564383+Musallamjaw@users.noreply.github.com>
2025-03-29 17:51:43 +0300

    Update Event.model.js

--- server/models/Event.model.js
+++ server/models/Event.model.js
@@ -4,7 +4,27 @@ const EventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, required: true },
-  location: { type: String, required: true },
+  location: { 
+    type: String, 
+    required: function() { return this.eventType === 'in-person'; } 
+  },
+  meetingLink: { 
+    type: String, 
+    required: function() { return this.eventType === 'online'; },
+    validate: {
+      validator: function(v) {
+        // Simple URL validation
+        return this.eventType !== 'online' || /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(v);
+      },
+      message: props => `${props.value} is not a valid URL!`
+    }
+  },
+  eventType: { 
+    type: String, 
+    required: true, 
+    enum: ['in-person', 'online'],
+    default: 'in-person'
+  },
   price: { type: Number, required: true },
   capacity: { type: Number, required: true },
   availableTickets: { type: Number, required: true },
