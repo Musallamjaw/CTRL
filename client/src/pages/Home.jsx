e9db116130c806200864a505ce16e0ba976d244b
Praveenkurupati <138180628+Praveenkurupati@users.noreply.github.com>
2025-05-01 00:07:17 +0530

    feat: `Refactor Home component layout for Latest Blogs section with improved styling`

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -134,20 +134,20 @@ export default function Home() {
       </section>
 
       <AboutUs />
-
-      <div className="max-w-[1300px] mx-auto px-4 py-10">
-        <h2 className="text-2xl md:text-4xl font-bold text-center text-gray-800 mb-8">
-          Latest Blogs
-        </h2>
-        {/* Container for the feed */}
-        <div className="max-w-5xl mx-auto px-4 space-y-8 md:space-y-12">
-          {/* Map over blogs and render each card */}
-          {blogs.map((blog) => (
-            <ClientBlogCard key={blog._id} blog={blog} />
-          ))}
+      <div className="bg-gray-100">
+        <div className="max-w-[1300px] mx-auto px-4 py-10">
+          <h2 className="text-2xl md:text-4xl font-bold text-center text-gray-800 mb-8">
+            Latest Blogs
+          </h2>
+          {/* Container for the feed */}
+          <div className="max-w-5xl mx-auto px-4 space-y-8 md:space-y-12">
+            {/* Map over blogs and render each card */}
+            {blogs.map((blog) => (
+              <ClientBlogCard key={blog._id} blog={blog} />
+            ))}
+          </div>
         </div>
       </div>
-
       <section
         id="events"
         className="relative bg-ticket-bg bg-no-repeat bg-cover bg-center w-full"
